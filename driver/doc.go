272a10a847417d87ecf19bf45caf35e956cf25a3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-06 16:38:01 -0700

    docs

--- driver/doc.go
+++ driver/doc.go
@@ -1,4 +1,23 @@
 /*
 Package digestfs_driver provides tools for registering content-addressable file system drivers for the virtual file system (VFS) for package digestfs.
+
+Drivers
+
+To create a driver, do something like the following:
+
+	import "github.com/reiver/go-digestfs/driver"
+	
+	func init() {
+		
+		var name string
+		
+		var mounter MyMounter
+		
+		// ...
+		
+		if err := digestfs_driver.Registry.Register(mounter, name); nil != err {
+			panic(err)
+		}
+	}
 */
 package digestfs_driver
