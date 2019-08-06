4226867092f3a1289009a7747d2c82fd78ba2e2b
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-06 15:31:49 -0700

    docs

---
+++ driver/doc.go
@@ -0,0 +1,4 @@
+/*
+Package digestfs_driver provides tools for registering content-addressable file system drivers for the virtual file system (VFS) for package digestfs.
+*/
+package digestfs_driver
