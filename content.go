657c07e4ac31131bacddd49a41b94adc3173d45e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-07 09:25:52 -0700

    docs

--- content.go
+++ content.go
@@ -6,7 +6,31 @@ import (
 
 // Content provides a read-only interface to content.
 //
+// Example
 //
+// Here is an example of digestfs.Content being used.
+//
+//	content, err := Open(algorithm, digest)
+//	if nil != err {
+//		return err
+//	}
+//	defer content.Close()
+//	
+//	// ...
+//	
+//	var buffer [128]byte
+//	p := buffer[:]
+//	
+//	n, err := content.ReadAt(p, offset)
+//
+// Reader, Seeker
+//
+// If you want to turn Content into a io.Reader, and io.Seeker, you can
+// do that by wrapping it in a io.SectionReader.
+//
+// For example:
+//
+//	r := io.NewSectionReader(content, 0, content.Len())
 type Content interface {
 	io.Closer
 	io.ReaderAt
