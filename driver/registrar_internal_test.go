e26e45930f2d781396096ef3795c718427ee66da
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-06 17:42:21 -0700

    digestfs_driver.internalRegistrar

--- driver/registrar_internal_test.go
+++ driver/registrar_internal_test.go
@@ -43,4 +43,56 @@ func TestInternalRegistrar(t *testing.T) {
 			continue
 		}
 	}
+
+	for testNumber, name := range names {
+		var mounter Mounter = MounterFunc(func(src string, params ...interface{}) (Opener, error){
+			return nil, nil
+		})
+
+		if err := registrar.Register(mounter, name); nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %q", testNumber, err, err)
+			continue
+		}
+	}
+
+	for testNumber, name := range names {
+		mounter, err := registrar.Fetch(name)
+		if nil != err {
+			t.Errorf("For test #%d, did not expect an error, but actually got one: (%T) %q", testNumber, err, err)
+			t.Logf("Name: %q", name)
+			continue
+		}
+		if nil == mounter {
+			t.Errorf("For test #%d, expected a mounter, but did not actually get one: %#v", testNumber, mounter)
+			t.Logf("Name: %q", name)
+			continue
+		}
+	}
+
+	{
+		var otherNames []string = []string{
+			"StrawBerry",
+			"Watermelon",
+		}
+
+		for testNumber, name := range otherNames {
+			mounter, err := registrar.Fetch(name)
+			if nil == err {
+				t.Errorf("For test #%d, expected to get an error, but actually did not: %#v", testNumber, err)
+				t.Logf("Name: %q", name)
+				continue
+			}
+			if nil != mounter {
+				t.Errorf("For test #%d, did not expect a mounter, but actually got one: %#v", testNumber, mounter)
+				t.Logf("Name: %q", name)
+				continue
+			}
+
+			if _, casted := err.(NotFound); !casted {
+				t.Errorf("For test #%d, expected ‘not found’ error, but did not actually get it: (%T) %q", testNumber, err, err)
+				t.Logf("Name: %q", name)
+				continue
+			}
+		}
+	}
 }
