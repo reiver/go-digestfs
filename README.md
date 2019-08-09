554e828d606a865d1902cc5461510666d15f789d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-09 07:49:09 -0700

    docs

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ var mountpoint digestfs.MountPoint
 // ...
 
 content, err := mountpoint.Open("SHA-256", "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a")
+if nil != err {
+	return err
+}
+defer content.Close()
 ```
 
 ## Content Addressing
--- doc.go
+++ doc.go
@@ -11,6 +11,10 @@ Here is an example of how to use an already mounted digestfs.MountPoint to get c
 	// ...
 	
 	content, err := mountpoint.Open("SHA-256", "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a")
+	if nil != err {
+		return err
+	}
+	defer content.Close()
 
 Here, ‘content’ would give you access to the data on the content mostly via an io.ReaderAt interface.
 For example:
