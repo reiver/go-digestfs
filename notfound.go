05692269a064d54a725ca417ed4b5cd417f6b2c4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-09 13:58:33 -0700

    removed digestfs.NotFound, and split into digestfs.ContentNotFound, and digestfs.MounterNotFound

--- notfound.go
+++ notfound.go
@@ -4,29 +4,14 @@ import (
 	"fmt"
 )
 
-type NotFound interface {
+// ContentNotFound may be returned by MountPoint.Open(), and MountPoint.OpenLocation().
+type ContentNotFound interface {
 	error
-	NotFound() (algorithm string, digest string)
+	ContentNotFound() (algorithm string, digest string)
 }
 
-func errNotFound(algorithm string, digest string) error {
-	var e NotFound = &internalNotFound{
-		algorithm: algorithm,
-		digest:    digest,
-	}
-
-	return e
-}
-
-type internalNotFound struct {
-	algorithm string
-	digest string
-}
-
-func (receiver internalNotFound) Error() string {
-	return fmt.Sprintf("digestfs: Not Found: algorithm=%q digest=%q", receiver.algorithm, receiver.digest)
-}
-
-func (receiver internalNotFound) NotFound() (algorithm string, digest string) {
-	return receiver.algorithm, receiver.digest
+// MounterNotFound may be returned by Mounter.Mount().
+type MounterNotFound interface {
+	error
+	MounterNotFound() (algorithm string, digest string)
 }
