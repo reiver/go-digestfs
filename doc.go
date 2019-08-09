9a124bfb12a3fba80b04016c29d9466a4dfcfc0d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-09 07:45:58 -0700

    docs

--- doc.go
+++ doc.go
@@ -11,5 +11,25 @@ Here is an example of how to use an already mounted digestfs.MountPoint to get c
 	// ...
 	
 	content, err := mountpoint.Open("SHA-256", "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a")
+
+Here, ‘content’ would give you access to the data on the content mostly via an io.ReaderAt interface.
+For example:
+
+	var buffer [256]byte
+
+	var p []byte = buffer[:]
+
+	var offset int64 = 0
+	
+	n, err := content.ReadAt(p, offset)
+
+Alternatively, if you want to use the io.Reader interface, you can upgrade ‘content’ from an io.ReaderAt to an io.Reader by using an io.SectionReader.
+For example:
+
+	r := io.NewSectionReader(content, 0, content.Len())
+	
+	// ...
+	
+	contentBytes, err := ioutil.RealAll(r)
 */
 package digestfs
