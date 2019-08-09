c5c5700c50e3eaed66cb270c948be3f46417b3d1
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-09 14:08:48 -0700

    docs

--- notfound.go
+++ notfound.go
@@ -5,12 +5,48 @@ import (
 )
 
 // ContentNotFound may be returned by MountPoint.Open(), and MountPoint.OpenLocation().
+//
+// Example
+//
+// Here is an example usage:
+//
+//	var mountpoint digestfs.MountPoint
+//	
+//	// ...
+//	
+//	content, err := mountpoint.Open("SHA-256", "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")
+//	
+//	if nil != err {
+//		switch err.(type) {
+//		case digestfs.ContentNotFound:
+//			//@TODO
+//		default:
+//			//@TODO
+//		}
+//	}
 type ContentNotFound interface {
 	error
 	ContentNotFound() (algorithm string, digest string)
 }
 
 // MounterNotFound may be returned by Mounter.Mount().
+//
+// Example
+//
+// Here is an example usage:
+//
+//	var mountpoint digestfs.MountPoint
+//	
+//	err := mountpoint.Mount("git", "/home/me/workspaces/project")
+//
+//	if nil != err {
+//		switch err.(type) {
+//		case digestfs.MounterNotFound:
+//			//@TODO
+//		default:
+//			//@TODO
+//		}
+//	}
 type MounterNotFound interface {
 	error
 	MounterNotFound() (algorithm string, digest string)
