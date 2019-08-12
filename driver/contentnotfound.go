04ef74052c1cea0cbe6d73b807693173bbe352d5
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-11 22:41:32 -0700

    made digest string again. the intend is that .Open() has a human-readable digest.

--- driver/contentnotfound.go
+++ driver/contentnotfound.go
@@ -6,13 +6,13 @@ import (
 
 type ContentNotFound interface {
 	error
-	ContentNotFound() (algorithm string, digest []byte)
+	ContentNotFound() (algorithm string, digest string)
 }
 
-func ErrContentNotFound(algorithm string, digest []byte) error {
+func ErrContentNotFound(algorithm string, digest string) error {
 	var e ContentNotFound = &internalContentNotFound{
 		algorithm: algorithm,
-		digest: append([]byte(nil), digest...),
+		digest: digest,
 	}
 
 	return e
@@ -20,13 +20,13 @@ func ErrContentNotFound(algorithm string, digest []byte) error {
 
 type internalContentNotFound struct {
 	algorithm string
-	digest []byte
+	digest string
 }
 
 func (receiver internalContentNotFound) Error() string {
-	return fmt.Sprintf("digestfs: Content Not Found: algorithm=%q digest=%x", receiver.algorithm, receiver.digest)
+	return fmt.Sprintf("digestfs: Content Not Found: algorithm=%q digest=%q", receiver.algorithm, receiver.digest)
 }
 
-func (receiver internalContentNotFound) ContentNotFound() (string, []byte) {
+func (receiver internalContentNotFound) ContentNotFound() (string, string) {
 	return receiver.algorithm, receiver.digest
 }
