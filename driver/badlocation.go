ee1bee6d87d494f7d92fbc02341c3b8f289aeb35
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 09:41:21 -0700

    docs

--- driver/badlocation.go
+++ driver/badlocation.go
@@ -4,11 +4,43 @@ import (
 	"fmt"
 )
 
+// BadLocation is an error that should be returned by a MountPoint's OpenLocation() method when the
+// value of the ‘location’ parameter passed to OpenLocation(location string) is
+//
+// You can create this kind of error by calling the ErrBadLocation() function.
+// For example:
+//
+//	func (receiver MyMountPoint) OpenLocation(location string) (digestfs_driver.Content, error) {
+//
+//		// ...
+//
+//		if locationIsBad {
+//			nil, return digestfs_driver.ErrBadLocation(location)
+//		}
+//
+//		// ...
+//	}
+//
+// Note that this is a different error that ContentNotFound.
+//
+// The BadLocation Error means that the format of the value of ‘location’ is bad.
+// Ex: it is not understandable by your code.
+//
+// So, for example, if you decided that you driver will accept locations in this kind of format:
+//
+//	"@->/path/on/the/file/system/to/the/content"
+//
+// Then if OpenLocation() received something that didn't begin with a "@->" then it would return
+// a BadLocation error.
+//
+// If the format of the value of ‘location’ was good, but it just wasn't there, then you would
+// return a ContentNotFound error (using ErrContentNotFound()) instead.
 type BadLocation interface {
 	error
 	BadLocation() string
 }
 
+// ErrBadLocation returns a BadLocation error.
 func ErrBadLocation(location string) error {
 	var e BadLocation = &internalBadLocation{
 		location: location,
