73c5b4d33c871a6245e52c363ce78534352c4de3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 09:46:35 -0700

    docs

--- driver/badlocation.go
+++ driver/badlocation.go
@@ -72,15 +72,4 @@ func (receiver internalBadLocation) Error() string {
 
 func (receiver internalBadLocation) BadLocation() string {
 	return receiver.location
-//
-//	func (receiver MyMountPoint) OpenLocation(location string) (digestfs_driver.Content, error) {
-//
-//		// ...
-//
-//		if locationIsBad {
-//			nil, return digestfs_driver.ErrBadLocation(location)
-//		}
-//
-//		// ...
-//	}
 }
