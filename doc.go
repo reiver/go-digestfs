812d733fc88389f29645cc64002788c000102850
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 10:38:13 -0700

    docs

--- doc.go
+++ doc.go
@@ -16,10 +16,12 @@ Note that if you had the digest encoded as hexadecimal, then you could decode it
 As in:
 
 	var hexadecimalDigest = "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a"
-
+	
 	digestBytes, err := hex.DecodeString(hexadecimalDigest)
-
+	
 	var digest string = string(digestBytes)
+	
+	// digest == "\xc0\x53\x5e\x4b\xe2\xb7\x9f\xfd\x93\x29\x13\x05\x43\x6b\xf8\x89\x31\x4e\x4a\x3f\xae\xc0\x5e\xcf\xfc\xbb\x7d\xf3\x1a\xd9\xe5\x1a"
 
 Here, ‘content’ would give you access to the data on the content mostly via an io.ReaderAt interface.
 For example:
