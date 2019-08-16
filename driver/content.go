2ed4fee71d8b29bcbe39eac1ae7404cef389ac3f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 10:11:01 -0700

    docs

--- driver/content.go
+++ driver/content.go
@@ -7,6 +7,10 @@ import (
 // Content represents open content.
 //
 // Content is returned by Open(), and OpenLocation().
+//
+// MountPoints can use the StringContent() helper function create a Content.
+//
+// Alternatively, MountPoints can create this own implementation of Content.
 type Content interface {
 	io.Closer
 	io.ReaderAt
