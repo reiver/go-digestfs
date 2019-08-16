053b42c200bea5c6c2ddf416558bf5dd6503f41a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 16:16:58 -0700

    example

--- example_contentnotfound_test.go
+++ example_contentnotfound_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/reiver/go-digestfs"
 	"github.com/reiver/go-memdigest"
 
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -47,13 +48,16 @@ func ExampleContentNotFound() {
 
 
 	func(){
-		content, err := mountpoint.Open("SHA-1.hexadecimal", "d3486ae9136e7856bc42212385ea797094475802")
+		// 0xd3486ae9136e7856bc42212385ea797094475802
+		var digest string = "\xd3\x48\x6a\xe9\x13\x6e\x78\x56\xbc\x42\x21\x23\x85\xea\x79\x70\x94\x47\x58\x02"
+
+		content, err := mountpoint.Open("SHA-1", digest)
 		if nil != err {
 			switch err.(type) {
 			case digestfs.ContentNotFound: // <--------- This is where digestfs.ContentNotFound is being used
-				fmt.Print("ERROR: could not find content with SHA-1 digest “d3486ae9136e7856bc42212385ea797094475802”\n")
+				fmt.Printf("ERROR: could not find content with SHA-1 digest “%s”\n", hex.EncodeToString([]byte(digest)))
 			default:
-				fmt.Printf("ERROR: could not open content with SHA-1 digest “d3486ae9136e7856bc42212385ea797094475802” because: %s\n", err)
+				fmt.Printf("ERROR: could not open content with SHA-1 digest “%s” because: %s\n", hex.EncodeToString([]byte(digest)), err)
 			}
 			return
 		}
@@ -62,22 +66,24 @@ func ExampleContentNotFound() {
 		r := io.NewSectionReader(content, 0, int64(content.Len()))
 		contentBytes, err := ioutil.ReadAll(r)
 		if nil != err {
-			fmt.Printf("ERROR: could not read all bytes of content with SHA-1 digest “d3486ae9136e7856bc42212385ea797094475802” because: %s\n", err)
+			fmt.Printf("ERROR: could not read all bytes of content with SHA-1 digest “%s” because: %s\n", hex.EncodeToString([]byte(digest)), err)
 			return
 		}
 
-		fmt.Printf("Content with SHA-1 digest “d3486ae9136e7856bc42212385ea797094475802” is: %q\n", contentBytes)
+		fmt.Printf("Content with SHA-1 digest “%s” is: %q\n", hex.EncodeToString([]byte(digest)), contentBytes)
 	}()
 
-
 	func(){
-		content, err := mountpoint.Open("SHA-1.hexadecimal", "80256f39a9d308650ac90d9be9a72a9562454574")
+		// 0x80256f39a9d308650ac90d9be9a72a9562454574
+		var digest string = "\x80\x25\x6f\x39\xa9\xd3\x08\x65\x0a\xc9\x0d\x9b\xe9\xa7\x2a\x95\x62\x45\x45\x74"
+
+		content, err := mountpoint.Open("SHA-1", digest)
 		if nil != err {
 			switch err.(type) {
 			case digestfs.ContentNotFound: // <--------- This is where digestfs.ContentNotFound is being used
-				fmt.Print("ERROR: could not find content with SHA-1 digest “80256f39a9d308650ac90d9be9a72a9562454574”\n")
+				fmt.Printf("ERROR: could not find content with SHA-1 digest “%s”\n", hex.EncodeToString([]byte(digest)))
 			default:
-				fmt.Printf("ERROR: could not open content with SHA-1 digest “80256f39a9d308650ac90d9be9a72a9562454574” because: %s\n", err)
+				fmt.Printf("ERROR: could not open content with SHA-1 digest “%s” because: %s\n", hex.EncodeToString([]byte(digest)), err)
 			}
 			return
 		}
@@ -86,11 +92,11 @@ func ExampleContentNotFound() {
 		r := io.NewSectionReader(content, 0, int64(content.Len()))
 		contentBytes, err := ioutil.ReadAll(r)
 		if nil != err {
-			fmt.Printf("ERROR: could not read all bytes of content with SHA-1 digest “80256f39a9d308650ac90d9be9a72a9562454574” because: %s\n", err)
+			fmt.Printf("ERROR: could not read all bytes of content with SHA-1 digest “%s” because: %s\n", hex.EncodeToString([]byte(digest)), err)
 			return
 		}
 
-		fmt.Printf("Content with SHA-1 digest “80256f39a9d308650ac90d9be9a72a9562454574” is: %q\n", contentBytes)
+		fmt.Printf("Content with SHA-1 digest “%s” is: %q\n", hex.EncodeToString([]byte(digest)), contentBytes)
 	}()
 
 
