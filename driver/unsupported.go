5dc4ca32318ab5804413d9811be6261640274b4e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-16 10:22:50 -0700

    docs

--- driver/unsupported.go
+++ driver/unsupported.go
@@ -4,11 +4,41 @@ import (
 	"fmt"
 )
 
+// UnsupportedAlgorithm is an error that should be returned by a MountPoint's Open() method
+// when the algorithm specified by value of the ‘algorithm’ is not supported.
+//
+// You can create this kind of error by calling the ErrUnsupportedAlgorithm() function.
+// For example:
+//
+//	func (receiver MyMountPoint) Open(algorithm string, digest string) (digestfs_driver.Content, error) {
+//
+//		// ...
+//
+//		if algorithmNotSupported {
+//			nil, return digestfs_driver.ErrUnsupportedAlgorithm(algorithm)
+//		}
+//
+//		// ...
+//	}
 type UnsupportedAlgorithm interface {
 	error
 	UnsupportedAlgorithm() string
 }
 
+// ErrUnsupportedAlgorithm returns a UnsupportedAlgorithm error.
+//
+// Here is how one might use this function:
+//
+//	func (receiver MyMountPoint) Open(algorithm string, digest string) (digestfs_driver.Content, error) {
+//
+//		// ...
+//
+//		if algorithmNotSupported {
+//			nil, return digestfs_driver.ErrUnsupportedAlgorithm(algorithm)
+//		}
+//
+//		// ...
+//	}
 func ErrUnsupportedAlgorithm(name string) error {
 	var e UnsupportedAlgorithm = &internalUnsupportedAlgorithm{
 		name: name,
